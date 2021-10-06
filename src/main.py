46782c7faf3ccd739bbe4f67f95190a36b161919
axellopez89 <axellpz89@hotmail.com>
2021-10-06 09:22:17 -0400

    Fixed main

--- src/main.py
+++ src/main.py
@@ -1,33 +1,4 @@
-from time import perf_counter as timer
 
-def Anagrams(wordlist):
-    anagrams = {}
-
-    for word in range(len(wordlist)):
-        lowercased = wordlist[word].lower()
-        sortedWord = "".join(sorted(lowercased))
-        if sortedWord in anagrams:
-            anagrams[sortedWord].append(lowercased)
-        else:
-            anagrams[sortedWord] = [lowercased]
-
-    return anagrams
-
-
-filename = "src\wordlist.txt"
-file = open(filename, "r")
-lines = file.readlines()
-words = []
-
-for word in lines:
-    words.append(word.replace("\n", ""))
-start = timer()
-anagrams = Anagrams(words)
-# for ana in anagrams:
-#     if len(anagrams[ana]) > 1:
-#         print(anagrams[ana])
-end = timer()
-print(end - start)
 
 
 
