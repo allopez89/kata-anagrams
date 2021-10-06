b1bcda396af21a7966d76c6692509c288c070097
axellopez89 <axellpz89@hotmail.com>
2021-10-06 09:49:36 -0400

    Test for int list to return a warning

--- src/AnagramApp.py
+++ src/AnagramApp.py
@@ -1,14 +1,16 @@
 def Anagrams(wordlist):
-    anagrams = {}
+    if all(type(n) == str for n in wordlist):
+        anagrams = {}
 
-    for word in range(len(wordlist)):
-        lowercased = wordlist[word].lower()
-        sortedWord = "".join(sorted(lowercased))
-        if sortedWord in anagrams:
-            anagrams[sortedWord].append(lowercased)
-        else:
-            anagrams[sortedWord] = [lowercased]
+        for word in range(len(wordlist)):
+            lowercased = wordlist[word].lower()
+            sortedWord = "".join(sorted(lowercased))
+            if sortedWord in anagrams:
+                anagrams[sortedWord].append(lowercased)
+            else:
+                anagrams[sortedWord] = [lowercased]
 
-    return len(anagrams)
-
-
+        return len(anagrams)
+    else:
+        return "The list must contain strings!"
+    
--- src/test.py
+++ src/test.py
@@ -5,4 +5,7 @@ class TestAnagrams(unittest.TestCase):
     def test_empty_list_returns_0_anagrams(self):
         self.assertEqual(Anagrams([]), 0)
 
+    def test_int_list_returns_warning(self):
+        self.assertEqual(Anagrams([11, 11, 12, 21, 13, 31]), "The list must contain strings!")
+
 unittest.main()
