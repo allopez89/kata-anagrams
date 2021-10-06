bb44016319033cc3b52f23a5adc0279aed793323
axellopez89 <axellpz89@hotmail.com>
2021-10-06 10:42:31 -0400

    Wordfile returns 30404 anagrams

--- src/AnagramApp.py
+++ src/AnagramApp.py
@@ -1,3 +1,4 @@
+from time import perf_counter as timer
 def Anagrams(wordlist):
     if all(type(n) == str for n in wordlist):
         anagrams = {}
@@ -9,8 +10,23 @@ def Anagrams(wordlist):
                 anagrams[sortedWord].append(lowercased)
             else:
                 anagrams[sortedWord] = [lowercased]
-
-        return len(anagrams)
+        
+        return len([ana for ana in anagrams if len(anagrams[ana]) > 1])
     else:
         return "The list must contain strings!"
+
+def ReadFile(filename):
+    file = open(filename, "r")
+    lines = file.readlines()
+    words = []
+
+    for word in lines:
+        words.append(word.replace("\n", ""))
+    return words
+    
+def Timer():
+    start = timer()
+    Anagrams(ReadFile("src\wordlist.txt"))
+    end = timer()
+    print(end - start)
     
--- src/test.py
+++ src/test.py
@@ -1,5 +1,5 @@
 import unittest
-from AnagramApp import Anagrams
+from AnagramApp import Anagrams, ReadFile
 
 class TestAnagrams(unittest.TestCase):
     def test_empty_list_returns_0_anagrams(self):
@@ -7,5 +7,8 @@ class TestAnagrams(unittest.TestCase):
 
     def test_int_list_returns_warning(self):
         self.assertEqual(Anagrams([11, 11, 12, 21, 13, 31]), "The list must contain strings!")
+
+    def test_wordlist_file_returns_30404_anagrams(self):
+        self.assertEqual(Anagrams(ReadFile("src\wordlist.txt")), 30404)
 
 unittest.main()
